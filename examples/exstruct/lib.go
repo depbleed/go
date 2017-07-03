0f8e6fb28c35fee17f20b50d046320bd6ec8b861
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2017-07-03 15:46:17 -0400

    add classical type export to the struct examples

--- examples/exstruct/lib.go
+++ examples/exstruct/lib.go
@@ -9,4 +9,12 @@ type MyType struct {
 	// A is a public member of a type provided by a dependency. This is
 	// dependency bleeding.
 	A a.Type
+	//B is a public member of a type provided by Go; nothing wrong here.
+	B string
+	//C is a public member of a type by this package; nothing wrong here.
+	C MyOtherType
+}
+
+//MyOtherType is another type exposed here
+type MyOtherType struct {
 }
