fedfc62fff913770dbeb43bff0de18b48be7ed95
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2017-07-11 11:56:26 -0400

    add coments on examples

--- examples/excomplete/lib.go
+++ examples/excomplete/lib.go
@@ -4,24 +4,32 @@ import (
 	"a"
 )
 
+//A is an exported standard type
 const A a.Int = 1
 
-var B a.Struct = a.Struct{}
+//B is an exported vendor type
+var B = a.Struct{}
 
+//C is an exported struct that exports a standard type
 type C struct {
 	D a.Time
 }
 
+//E is an exported interface that mixes vendor & standard type
 type E interface {
 	F(a.Int) a.Time
 }
 
+//G exports a standard type
 type G interface {
 	a.Interface
 }
 
+//H is standard type chan
 var H chan a.Int
 
+//I is a standard type pointer
 var I *a.Time
 
+//J is an exported standard type
 var J a.Int = 1
