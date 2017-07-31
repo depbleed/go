b9f173bd0e2423aee1f8f3dd33b53bc07d872aa9
Julien Kauffmann <julien.kauffmann@freelan.org>
2017-07-30 23:06:59 -0400

    Added test in excomplete

--- examples/excomplete/lib.go
+++ examples/excomplete/lib.go
@@ -1,4 +1,4 @@
-package exstruct
+package excomplete
 
 import (
 	"a"
---
+++ examples/excomplete/lib_test.go
@@ -0,0 +1,12 @@
+package excomplete
+
+import (
+	"a"
+	"testing"
+)
+
+func MyTestHelper(a.Int) {
+}
+
+func TestFoo(t *testing.T) {
+}
